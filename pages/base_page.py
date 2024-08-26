b4c6828373fe4e979cc68274626bfd25b27bef53
creamberry <creamberry@ya.ru>
2024-08-27 02:42:11 +0300

    new folder added; test file changed

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,17 +6,3 @@ class BasePage:
 
     def open(self):
         self.browser.get(self.url)
-
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException:
-            return False
-        return True
-
-    def is_element_visible(self, how, what):
-        try:
-            element = self.driver.find_element(by=how, value=what)
-            return element.is_displayed()
-        except StaleElementReferenceException:
-            return False
